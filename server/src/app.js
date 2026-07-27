b73d5469df6413ef85866eb143cbbb6c29cd6568
Dwarkadhish <dwarkadhishkapse@gmail.com>
2026-07-27 11:34:53 +0530

    feat: connect prisma client with express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,18 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-export default app
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is running",
+  });
+});
+
+export default app;
